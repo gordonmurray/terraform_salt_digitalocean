bb4ad3e8da3d802300fde4fc15bbe5a8069676f8
Gordon Murray <gordon@gordonmurray.com>
2018-09-08 10:06:00 +0100

    Adding comments

--- droplet.tf
+++ droplet.tf
@@ -1,23 +1,28 @@
+# get the pre made salt master image
 data "digitalocean_image" "salt" {
   name = "salt"
 }
 
+# get the pre made salt minion image
 data "digitalocean_image" "salt-minion" {
   name = "salt-minion"
 }
 
+# generate a short random string
 resource "random_string" "first" {
   length  = 8
   special = false
   upper   = false
 }
 
+# generate a short random string
 resource "random_string" "second" {
   length  = 8
   special = false
   upper   = false
 }
 
+# generate a short random string
 resource "random_string" "third" {
   length  = 8
   special = false
@@ -42,7 +47,7 @@ module "salt" {
   remote_exec_command = "salt-key -A -y"
 }
 
-# create salt minion
+# create salt minion with a unique hostname
 module "salt-minion-1" {
   source             = "./modules/digitalocean/droplet"
   image              = "${data.digitalocean_image.salt-minion.image}"
@@ -54,13 +59,15 @@ module "salt-minion-1" {
   ssh_keys           = ["${module.my_ssh_key.ssh_fingerprint}"]
   private_networking = "true"
 
+  # add the master IP address and the minions host name to a salt config file
   content     = "master: ${module.salt.droplet_ipv4_private}\nid: salt-minion-${random_string.first.result}"
   destination = "/etc/salt/minion"
 
+  # restart salt-minion on the host to take in the config file change
   remote_exec_command = "sudo service salt-minion restart"
 }
 
-# create salt minion
+# create salt minion with a unique hostname
 module "salt-minion-2" {
   source             = "./modules/digitalocean/droplet"
   image              = "${data.digitalocean_image.salt-minion.image}"
@@ -72,13 +79,15 @@ module "salt-minion-2" {
   ssh_keys           = ["${module.my_ssh_key.ssh_fingerprint}"]
   private_networking = "true"
 
+  # add the master IP address and the minions host name to a salt config file
   content     = "master: ${module.salt.droplet_ipv4_private}\nid: salt-minion-${random_string.second.result}"
   destination = "/etc/salt/minion"
 
+  # restart salt-minion on the host to take in the config file change
   remote_exec_command = "sudo service salt-minion restart"
 }
 
-# create salt minion
+# create salt minion with a unique hostname
 module "salt-minion-3" {
   source             = "./modules/digitalocean/droplet"
   image              = "${data.digitalocean_image.salt-minion.image}"
@@ -90,8 +99,10 @@ module "salt-minion-3" {
   ssh_keys           = ["${module.my_ssh_key.ssh_fingerprint}"]
   private_networking = "true"
 
+  # add the master IP address and the minions host name to a salt config file
   content     = "master: ${module.salt.droplet_ipv4_private}\nid: salt-minion-${random_string.third.result}"
   destination = "/etc/salt/minion"
 
+  # restart salt-minion on the host to take in the config file change
   remote_exec_command = "sudo service salt-minion restart"
 }
--- readme.md
+++ readme.md
@@ -1,20 +1,24 @@
 Terraform Salt DigitalOcean
 ---------------------------
 
-A project to set up a small Salt cluster on DigitalOcean, using Terraform.
+A project to set up a small Salt cluster on DigitalOcean, using Terraform, with 1 Salt master and 1 or more Salt minions.
 
 The images used to create the Droplets are premade using Packer.
 
 You will need to add your DigitalOcean API key to the variables.tfvars file for Terraform and to the packer/variables.json file for Packer.
 
-Terraform Plan
+Terraform Plan - to see the planned changes
 
 > terraform plan -var-file="variables.tfvars"
 
-Terraform Apply
+Terraform Apply - to apply the changes
 
 > terraform apply -var-file="variables.tfvars"
 
-Terraform Destroy
+Terraform show - to see the finished instances
+
+> terraform apply -var-file="variables.tfvars"
+
+Terraform Destroy - to remove all instances at the end
 
 > terraform destroy -var-file="variables.tfvars"
